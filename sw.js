ddbb262996251a15044781152be6a1a8fa8b8297
basarulusoy-bit <basarulusoy@gmail.com>
2026-04-16 03:30:01 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'arslanlar-v1';
+const ASSETS = ['/acenta.html', '/admin.html', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
